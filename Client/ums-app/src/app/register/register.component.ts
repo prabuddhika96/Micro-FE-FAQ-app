8ba3c1a66144ada0bf959317953cbe24022873c0
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-03-01 17:13:20 +0530

    add register page

--- Client/ums-app/src/app/register/register.component.ts
+++ Client/ums-app/src/app/register/register.component.ts
@@ -1,16 +1,59 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      register works!
-    </p>
-  `,
-  styleUrl: './register.component.css'
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  constructor() {}
 
+  editProfileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  errorMessages: any = {}; // Initialize errorMessages as an empty object
+
+  handleSubmit(event: Event) {
+    event.preventDefault();
+    // Clear previous error messages
+    this.errorMessages = {};
+
+    if (this.editProfileForm.valid) {
+      console.log(this.editProfileForm.value);
+    } else {
+      this.editProfileForm.markAllAsTouched();
+
+      // Iterate through each form control to check for validation errors
+      Object.keys(this.editProfileForm.controls).forEach((key) => {
+        const controlErrors = this.editProfileForm.get(key)?.errors;
+        if (controlErrors != null) {
+          Object.keys(controlErrors).forEach((keyError) => {
+            switch (keyError) {
+              case 'required':
+                this.errorMessages[key] = `${key} is required`;
+                break;
+              case 'email':
+                this.errorMessages[key] = `${key} must be a valid email`;
+                break;
+              default:
+                break;
+            }
+          });
+        }
+      });
+    }
+  }
 }
