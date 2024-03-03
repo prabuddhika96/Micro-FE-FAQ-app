46aebffef95eef853b45d73490cf384a8d873517
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-03-03 17:56:35 +0530

    add seperate css files

--- Client/faq-app/src/components/Header.tsx
+++ Client/faq-app/src/components/Header.tsx
@@ -4,21 +4,22 @@ import logo from "../Assets/Images/logo_main.png";
 import { useNavigate } from "react-router-dom";
 import { getRoute } from "../utility/function";
 import { RouteNames } from "../constants/RouteNames";
+import "../styles/Header_new.css"; // Import the CSS file
 
 const Header = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="bg-sky-200 sticky top-0 mx-auto !z-40 flex w-full items-center justify-between border-b border-gray-500 p-3 ">
+    <header className="header-container">
       <img
         src={logo}
         alt="logo"
-        className="w-10 h-10 mr-2 cursor-pointer"
+        className="logo-img"
         onClick={() => {
           navigate(getRoute(RouteNames.Home));
         }}
       />
-      <h1 className="text-5xl text-cyan-800  font-bold font-mono">QuePax</h1>
+      <h1 className="logo-text">QuePax</h1>
       <MyProfileComponent />
     </header>
   );
