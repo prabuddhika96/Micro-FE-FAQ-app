bd444894c0d8e0d14f9070238101a762c22797f3
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-03-04 10:09:14 +0530

    create utility project

--- Client/utility/src/eyepax-utility.js
+++ Client/utility/src/eyepax-utility.js
@@ -1,20 +1,2 @@
 // Anything exported from this file is importable by other in-browser modules.
-const apiCache = new Map();
-
-export function getData(url) {
-  const data = apiCache.get(url);
-  if (data) {
-    console.log("cached api");
-    return Promise.resolve(data);
-  }
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const result = {
-        data: 10,
-      };
-      apiCache.set(url, result);
-      console.log("actual api");
-      resolve(result);
-    }, 2000);
-  });
-}
+export function publicApiFunction() {}
