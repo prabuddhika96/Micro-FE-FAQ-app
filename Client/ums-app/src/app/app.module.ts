50b9c14b55d33908a7f09e0e977455754a9d17fc
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-03-05 11:04:31 +0530

    add new ui

--- Client/ums-app/src/app/app.module.ts
+++ Client/ums-app/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   // declarations: [AppComponent, EmptyRouteComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Client/ums-app/src/app/register/register.component.ts
+++ Client/ums-app/src/app/register/register.component.ts
@@ -1,59 +1,106 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [FormsModule, HttpClientModule, RouterModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  constructor() {}
+  // constructor() {}
 
-  editProfileForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required),
-  });
+  // editProfileForm = new FormGroup({
+  //   firstName: new FormControl('', Validators.required),
+  //   lastName: new FormControl('', Validators.required),
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   password: new FormControl('', Validators.required),
+  // });
 
-  errorMessages: any = {}; // Initialize errorMessages as an empty object
+  // errorMessages: any = {}; // Initialize errorMessages as an empty object
 
-  handleSubmit(event: Event) {
-    event.preventDefault();
-    // Clear previous error messages
-    this.errorMessages = {};
+  // handleSubmit(event: Event) {
+  //   event.preventDefault();
+  //   // Clear previous error messages
+  //   this.errorMessages = {};
 
-    if (this.editProfileForm.valid) {
-      console.log(this.editProfileForm.value);
-    } else {
-      this.editProfileForm.markAllAsTouched();
+  //   if (this.editProfileForm.valid) {
+  //     console.log(this.editProfileForm.value);
+  //   } else {
+  //     this.editProfileForm.markAllAsTouched();
 
-      // Iterate through each form control to check for validation errors
-      Object.keys(this.editProfileForm.controls).forEach((key) => {
-        const controlErrors = this.editProfileForm.get(key)?.errors;
-        if (controlErrors != null) {
-          Object.keys(controlErrors).forEach((keyError) => {
-            switch (keyError) {
-              case 'required':
-                this.errorMessages[key] = `${key} is required`;
-                break;
-              case 'email':
-                this.errorMessages[key] = `${key} must be a valid email`;
-                break;
-              default:
-                break;
-            }
-          });
+  //     // Iterate through each form control to check for validation errors
+  //     Object.keys(this.editProfileForm.controls).forEach((key) => {
+  //       const controlErrors = this.editProfileForm.get(key)?.errors;
+  //       if (controlErrors != null) {
+  //         Object.keys(controlErrors).forEach((keyError) => {
+  //           switch (keyError) {
+  //             case 'required':
+  //               this.errorMessages[key] = `${key} is required`;
+  //               break;
+  //             case 'email':
+  //               this.errorMessages[key] = `${key} must be a valid email`;
+  //               break;
+  //             default:
+  //               break;
+  //           }
+  //         });
+  //       }
+  //     });
+  //   }
+  // }
+
+  signupObj: Signup;
+  issubmit: boolean = false;
+  constructor(private http: HttpClient, private router: Router) {
+    this.signupObj = new Signup();
+  }
+  onSignUp() {
+    console.log('Login object:', this.signupObj);
+    this.issubmit = true;
+    // const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('logintoken')}`);
+
+    this.http
+      .post<any>('https://localhost:7288/api/UserAPI', this.signupObj)
+      .subscribe(
+        (response: any) => {
+          console.log('Response from server:', response);
+          if (response) {
+            alert('signUp successful');
+            this.router.navigateByUrl('/login');
+          }
+        },
+        (error: any) => {
+          if (error.error.msg) {
+            alert(error.error.msg);
+          } else {
+            alert('Make sure to fill all the fields correctly');
+          }
+          // alert(error.error.msg);
+          // console.log('Error:', error);
+          // this.router.navigateByUrl('/signup');
         }
-      });
-    }
+      );
+  }
+}
+
+export class Signup {
+  UserName: string;
+  FirstName: string;
+  LastName: string;
+  Email: string;
+  Password: string;
+  Role: string;
+  constructor() {
+    this.Email = '';
+    this.Password = '';
+    this.Role = 'user';
+    this.UserName = '';
+    this.LastName = '';
+    this.FirstName = '';
   }
 }
