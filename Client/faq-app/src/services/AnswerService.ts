998b85a72247a44d8c3ceedb8a54bd15db117eb5
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-03-05 15:47:41 +0530

    bind answer view

---
+++ Client/faq-app/src/services/AnswerService.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import authHeader from "../utility/auth-header";
+import { BACKEND_SERVER } from "../constants/BackEndServer";
+import authPostHeader from "../utility/post-header";
+
+const baseURL = BACKEND_SERVER;
+
+const addAnswer = async (data: any, questionId: any) => {
+  const response = await axios({
+    method: "post",
+    url: `${baseURL}/Faq/questions/${questionId}/answers`,
+    data: data,
+    headers: authPostHeader(),
+  });
+  // alert("Favourite created --- "+ response);
+  return response;
+};
+
+const AnswerService = {
+  addAnswer,
+};
+
+export default AnswerService;
